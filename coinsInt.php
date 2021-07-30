7d35a6e5479236b329e18cd776b86f5a6206202e
davework26 <56308896+davework26@users.noreply.github.com>
2021-07-30 17:41:50 +0100

    Significant algorithm improvements & annotation.
    Removed denominations main loop as only the largest denomination that fits in the remainder is needed.

--- coinsInt.php
+++ coinsInt.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * Calculate the minimum number of coins needed to satisfy an amount.
  * @param float $amount Monetary amount (up to 2 digit decimal) to convert to
  * minimum number of coins.
  * @return int Minimum number of coins needed to make up amount.
@@ -14,24 +14,42 @@ function calcCoins($amount)
     $coins = 0;
 
     if ($remainder > 0) {
-        foreach ($denominations as $denomination) {
-            echo "Denomination: $denomination\n<br \>";
-            echo "Remainder: $remainder\n<br \>";
-            echo "\n<br \>";
+        $denomination = findLargestDenominationInAmount($denominations, $remainder);
 
-            if ($denomination <= $remainder) {
-                $coins += intdiv($remainder, $denomination);
-                $remainder = $remainder % $denomination;
-                echo "Added denomination: $coins x $denomination\n<br \><br \>";
-                break;  // Found largest denomination so quit search foreach.
-            }
-        }
+        echo "Denomination: $denomination\n<br \>";
+        echo "Remainder: $remainder\n<br \>";
+        echo "\n<br \>";
+        $coins += intdiv($remainder, $denomination);
+        $remainder = $remainder % $denomination;
+        echo "Added denomination: $coins x $denomination\n<br \><br \>";
 
         return ($coins + calcCoins($remainder / 100));
     } else {
         return 0;   // Recursion terminating condition - fully converted, no remainder.
     }
 }
+
+/**
+ * Find the largest denomination that fits in the amount.
+ * E.g. 50 is the largest denomination that fits in the amount 52.
+ * @param array $denominations denominations.
+ * @param int $amount
+ * @return int the largest denomination in the amount.
+ */
+function findLargestDenominationInAmount($denominations, $amount)
+{
+    $largestDenom = 1;
+
+    foreach ($denominations as $denomination) {
+        if ($amount >= $denomination) {
+            $largestDenom = $denomination;
+            break;
+        }
+    }
+
+    return $largestDenom;
+}
+
 // Test cases:
 echo "Coins = " . calcCoins(0.3) . "\n<br /><br />";
 echo "Coins = " . calcCoins(.04) . "\n<br /><br />";
